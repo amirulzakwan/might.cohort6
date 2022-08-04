03148951117b39d17551ffe86eb845f16b442749
Ts. Amirul Zakwan Sarip <62842551+amirulzakwan@users.noreply.github.com>
2022-08-04 21:07:39 +0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -4,7 +4,10 @@ import numpy as np
 import pandas as pd
 
 st.header("My first Streamlit App")
-st.write(pd.DataFrame({
-    'Intplan': ['yes', 'yes', 'yes', 'no'],
-    'Churn Status': [0, 0, 0, 1]
-}))
+
+
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
